afadce334f42959425cf61d5e0877e00dcc95a26
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-05-31 02:27:22 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    // colors: {
+    //   current: "currentColor",
+    //   purplishblue: "#12c2e9",
+    //   skyblue: "#c471ed",
+    //   crystalblue: "#f7797d",
+    // },
   },
   plugins: [],
 };
