dc68c630c9827e20ceff17f08e824664c9c174fc
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-05-31 03:29:41 +0300

    .

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -31,7 +31,7 @@ function HeroSection() {
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-              Hire me
+              Contact me
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3 ">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
