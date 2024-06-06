fefbaf2f1eff67a417cd462489aae89293ed6cc0
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-06-06 13:17:18 +0300

    Update NavLink.jsx

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,9 +1,9 @@
 import { Link as ScrollLink } from "react-scroll";
 
-const NavLink = ({ href, title }) => {
+const NavLink = ({ path, title }) => {
   return (
     <ScrollLink
-      to={url}
+      to={path}
       smooth={true}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
